dea71b77e3f8c0dd080cb882c16e50c37c01f5fb
Christopher Webster <cajwebster03@gmail.com>
2023-03-22 11:23:31 -0300

    Read feeds config file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::io::BufRead;
+
 fn main() {
-    println!("Hello, world!");
+    let project_dirs = directories::ProjectDirs::from("", "cajwebster", "RSS Reader")
+        .expect("Could not find project directories");
+    let config_dir = project_dirs.config_dir();
+
+    std::fs::create_dir_all(config_dir).expect("Could not create config dir");
+
+    let feeds_path = config_dir.join("feeds");
+    let feeds = parse_feeds_file(
+        std::fs::OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .open(&feeds_path)
+            .unwrap_or_else(|e| panic!("Could not open feeds file {feeds_path:?}: {e}")),
+    );
+
+    println!("{feeds:?}");
+}
+
+fn parse_feeds_file(file: impl std::io::Read) -> Vec<(String, String)> {
+    let file = std::io::BufReader::new(file);
+    let mut feeds = vec![];
+
+    for line in file.lines() {
+        let line = line.expect("Failed to read line from feeds file");
+        let (name, url) = line.split_once(':').expect("Invalid format for feeds file");
+        feeds.push((name.to_string(), url.to_string()));
+    }
+
+    feeds
 }
